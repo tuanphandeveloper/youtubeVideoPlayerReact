cd4bf3b8f24f34c9c2570cfbd43b1019bbc995ba
Tuan Phan <tuan.p01@gmail.com>
2019-09-04 17:46:27 -0600

    Updates

---
+++ precache-manifest.0c02ec5de2b099dbf6424e441b6fe7e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e88651eddf10e9f2ef99f4922d586ce",
+    "url": "/youtubeVideoPlayerReact/index.html"
+  },
+  {
+    "revision": "5069559fcf2306d7f067",
+    "url": "/youtubeVideoPlayerReact/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "a8fdffc1b6e954d13691",
+    "url": "/youtubeVideoPlayerReact/static/js/2.50de70c3.chunk.js"
+  },
+  {
+    "revision": "5069559fcf2306d7f067",
+    "url": "/youtubeVideoPlayerReact/static/js/main.9b684704.chunk.js"
+  },
+  {
+    "revision": "259ffa30a25f33a29c03",
+    "url": "/youtubeVideoPlayerReact/static/js/runtime~main.a81fd934.js"
+  }
+]);
